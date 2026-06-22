cf690fc44d65c4a3d2679d4bef9e38830d472e22
izer <rezizavkaro@gmail.com>
2026-06-22 13:30:04 +0700

    Fix Change Repo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // Leave as '/' for Vercel or a custom domain / user-page GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: "Izer-SignatureApp",
+  base: "signature-engine",
   build: {
     target: "es2018",
     minify: "esbuild",

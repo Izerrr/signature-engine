d0fe304512505278ad176b9ba7d722955e15f663
izer <rezizavkaro@gmail.com>
2026-06-22 13:42:08 +0700

    Fixing Github Pages Config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // Leave as '/' for Vercel or a custom domain / user-page GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: "signature-engine",
+  base: "/signature-engine/",
   build: {
     target: "es2018",
     minify: "esbuild",
